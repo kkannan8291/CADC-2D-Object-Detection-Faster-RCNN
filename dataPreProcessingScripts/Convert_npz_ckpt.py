5aa2d3194ef64c4a08c77a5160b8efbbaf1e9d9c
kkanna17 <kkanna17@ford.com>
2021-11-15 00:44:47 -0500

    update

--- dataPreProcessingScripts/Convert_npz_ckpt.py
+++ dataPreProcessingScripts/Convert_npz_ckpt.py
@@ -10,7 +10,8 @@ path_to_npz ='/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN
 
 
 sess = tf.compat.v1.InteractiveSession()
-measure(lambda: tl.files.load_and_assign_npz(path_to_npz, sess), 'load npz')
+tl.files.load_npz(name=path_to_npz)
+#measure(lambda: tl.files.load_and_assign_npz(path_to_npz, sess), 'load npz')
 frozen_graph = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def, output_names)
 tf_model = tf.graph_util.remove_training_nodes(frozen_graph)
 uff_model = measure(lambda: uff.from_tensorflow(tf_model, output_names), 'uff.from_tensorflow')
