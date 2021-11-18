e28e616217943183fb1207495a15eff79b8bba5d
kkanna17 <kkanna17@ford.com>
2021-11-18 10:47:43 -0500

    updates

--- dataPreProcessingScripts/GenerateTxtFiles_PascalVOCData.py
+++ dataPreProcessingScripts/GenerateTxtFiles_PascalVOCData.py
@@ -4,20 +4,20 @@ from os.path import join
 
 if __name__ == '__main__':
     #source_folder =r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/VOC2007/JPEGImages'
-    source_folder =r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/data'
-    dest = r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/VOC2007/ImageSets/Main/test.txt'
-    #dest2 = r'G:\jianfeng\project\rubblish_det\source\train_pic_json\voc_all/VOC2018/ImageSets/Main/val.txt'
+    source_folder =r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/JPEGImages'
+    dest = r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/VOC2007/ImageSets/Main/trainval.txt'
+    dest2 = r'/home/kaushik/ObjectDetection/CADC-2D-Object-Detection-Faster-RCNN/data/VOC-Converted-Data/VOC2007/ImageSets/Main/test.txt'
     file_list = os.listdir(source_folder)
     train_file = open(dest, 'a')
-    #val_file = open(dest2, 'a')
+    test_file = open(dest2, 'a')
     i=0
     for file_obj in file_list:
         file_name, file_extend = os.path.splitext(file_obj)
 
-        #if (i%4 ==0):
-        #    val_file.write(file_name + '\n')
-        #else:
-        train_file.write(file_name + '\n')
+        if (i%8 ==0):
+            test_file.write(file_name + '\n')
+        else:
+            train_file.write(file_name + '\n')
         i+=1
     train_file.close()
-#val_file.close()
+test_file.close()
