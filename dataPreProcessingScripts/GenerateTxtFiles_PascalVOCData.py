f76b2368dcb0bbf5bf2f18d31652d7b960846f5b
kkanna17 <kkanna17@ford.com>
2022-03-14 22:36:18 -0400

    add car test file

--- dataPreProcessingScripts/GenerateTxtFiles_PascalVOCData.py
+++ dataPreProcessingScripts/GenerateTxtFiles_PascalVOCData.py
@@ -7,15 +7,18 @@ if __name__ == '__main__':
     source_folder =r'/home/kaushik/ObjectDetection/moco/detection/datasets/VOC2007/JPEGImages'
     dest = r'/home/kaushik/ObjectDetection/moco/detection/datasets/VOC2007/ImageSets/Main/trainval.txt'
     dest2 = r'/home/kaushik/ObjectDetection/moco/detection/datasets/VOC2007/ImageSets/Main/test.txt'
+    dest3 = r'/home/kaushik/ObjectDetection/moco/detection/datasets/VOC2007/ImageSets/Main/car_test.txt'
     file_list = sorted(os.listdir(source_folder))
     train_file = open(dest, 'a')
     test_file = open(dest2, 'a')
+    cartest_file = open(dest3, 'a')
     i=0
     for file_obj in file_list:
         file_name, file_extend = os.path.splitext(file_obj)
 
         if (i > 9999):
             test_file.write(file_name + '\n')
+            cartest_file.write(file_name + ' 1' + '\n')
         else:
             train_file.write(file_name + '\n')
         i+=1
